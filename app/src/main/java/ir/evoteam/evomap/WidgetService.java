d3c38405ddec79811186c38c1051724cc472aa3c
FarzadAkj <farzad2116@gmail.com>
2017-05-26 22:20:16 +0430

    adding a widget to this app mostafa's job is done

--- app/src/main/java/ir/evoteam/evomap/WidgetService.java
+++ app/src/main/java/ir/evoteam/evomap/WidgetService.java
@@ -2,8 +2,10 @@ package ir.evoteam.evomap;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 public class WidgetService extends Service {
 
@@ -25,7 +27,32 @@ public class WidgetService extends Service {
     }
     //commit
 
+
+
     public void changeStatus(String s){
-        //mostafa job
+
+        String TAG = "Mostafa" ;
+        SharedPreferences.Editor editor = MapsActivity.sharedPreferences.edit();
+        switch (s) {
+            case "onWay" :
+                editor.putInt(Constant.Driver_STATE_PREF_KEY, Constant.State_ONTHEWAY);
+                editor.commit();
+                Log.d(TAG, "changeStatus: " + "onWay" );
+                break ;
+            case "ready" :
+                editor.putInt(Constant.Driver_STATE_PREF_KEY, Constant.State_ONSERVICE);
+                editor.commit();
+                Log.d(TAG, "changeStatus: " + "ready" );
+                break ;
+            case "resting" :
+                editor.putInt(Constant.Driver_STATE_PREF_KEY, Constant.State_REST);
+                editor.commit();
+                Log.d(TAG, "changeStatus: " + "resting" );
+                break ;
+            default:
+                Log.d(TAG, "changeStatus: defaut");
+                break ;
+        }
     }
+
 }
